f9d0d5f490d65da85c959535656c18c715318b89
BayronVazquez <bayronnjonathan@gmail.com>
2022-04-03 18:27:09 -0500

    solucion al problema https://omegaup.com/arena/problem/Menor-a-Mayor/

---
+++ src/c/7332-BayronVazquez.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1, n2;
+    scanf("%i %i",&n1, &n2);
+    if(n1 < n2){
+        printf("%i\n",n1);
+        printf("%i\n",n2);
+    }else{
+        printf("%i\n",n2);
+        printf("%i\n",n1);
+    }
+
+    return 0;
+}
