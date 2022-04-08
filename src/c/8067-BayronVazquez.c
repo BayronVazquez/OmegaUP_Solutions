b452848d4ee5346bd596b6471c079ffa6bf57fff
Bayron Vazquez <bayronnjonathan@gmail.com>
2022-04-07 21:23:24 -0500

    solucion al problema https://omegaup.com/arena/problem/Leer-un-numero-e-imprimirlo

---
+++ src/c/8067-BayronVazquez.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1;
+    scanf("%i",&n1);
+    if(n1 > 0){
+        printf("%i\n",n1);
+    }else{
+        printf("Error");
+    }
+
+    return 0;
+}
